3b58331c5debd3b5870ceac59ee1306f3ab8aa80
Lisandrot1 <lmlisandro3@gmail.com>
2026-01-07 18:53:37 -0500

    feat: Terminamos por un momento con la extracion de paises, para iniciar con la transformacion de los paises igual.

--- src/utils/logging.py
+++ src/utils/logging.py
@@ -5,9 +5,13 @@ def logging_module(name: str = __name__):
     logger = logging.getLogger(name)
     
     if not logger.handlers:
-        logging.basicConfig(level= logging.INFO,
+        logging.basicConfig(
+            
+            level= logging.INFO,
             format="%(asctime)s %(name)s %(filename)s %(message)s",
             filename='logs.log',
-            filemode='a')
+            filemode='a'
+            
+            )
     
     return logger
